0bb8f0536c75821e3572492e74d6611b84806259
Noah Hendrickson <nhendrickson2001@gmail.com>
2023-01-23 12:45:18 -0600

    maybe

--- src/main_top_level.js
+++ src/main_top_level.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/src/Three.js';
+import * as THREE from './node_modules/three/src/Three.js';
 import WebGL from './webGLCheck.js';
 
 var scene, renderer, camera;
