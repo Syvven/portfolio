80676ca5a2101803651ecce467ce8125b5d1f9de
Noah Hendrickson <nhendrickson2001@gmail.com>
2023-01-24 19:50:42 -0600

    changed some link things and started on the graphics

--- src/main_top_level.js
+++ src/main_top_level.js
@@ -3,6 +3,20 @@ import WebGL from './webGLCheck.js';
 
 var scene, renderer, camera;
 
+/*
+    Next set of variables is for each year's projects
+*/
+var at_2020_pos = false;
+var moving_to_2020_pos = false;
+
+var look_x_2020 = 0;
+var look_y_2020 = 100;
+var look_z_2020 = 0;
+
+var pos_x_2020 = 10;
+var pos_y_2020 = 100;
+var pos_z_2020 = 10;
+
 function setup() 
 {
     /* 
@@ -25,13 +39,44 @@ function setup()
         0.1, 
         5000 
     );
+
     camera.position.set( 50, 50, 200 );
     camera.lookAt( 0, 0, 0 );
+}
+
+function update_camera_pos(dt)
+{
+    if (moving_to_2020_pos)
+    {
+        if (at_2020_pos) 
+        {
+            moving_to_2020_pos = false;
+            at_2020_pos = false;
+        }
+        else
+        {
+            camera.position.x = pos_x_2020;
+            camera.position.y = pos_y_2020;
+            camera.position.z = pos_z_2020;
+            camera.lookAt(
+                look_x_2020,
+                look_y_2020,
+                look_z_2020
+            );
+            at_2020_pos = true;
+        }
+    }
 }
 
 function animate() 
 {
     requestAnimationFrame( animate );
+
+    var now = new Date();
+    var dt = (now - prevTime) / 1000;
+    prevTime = now;
+
+    update_camera_pos(dt);
 
     renderer.render( scene, camera );
 }
@@ -46,7 +91,7 @@ function onWindowResize()
     renderer.render(scene, camera);
 }
 
-document.getElementById("click-me").addEventListener("click", (e) =>
+document.getElementById("about-me-card").addEventListener("click", (e) =>
 {
     document.getElementById("directory-ui").style.display = "none";
     document.getElementById("about-me-ui").style.display = "inline-block";
@@ -58,10 +103,17 @@ document.getElementById("return-card").addEventListener("click", (e) =>
     document.getElementById("about-me-ui").style.display = "none";
 });
 
+document.getElementById("card-2020").addEventListener("click", (e) =>
+{
+    document.getElementById("directory-ui").style.display = "none";
+    moving_to_2020_pos = true;
+});
+
 if ( WebGL.isWebGLAvailable() ) 
 {
     // Initiate function or other initializations here
     setup();
+    var prevTime = new Date();
     animate();
 } 
 else 
